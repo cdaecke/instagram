5d80f62bb9ed0c71325ea887a47d603e4d2f6bea
Alexander Kellner <alexander.kellner@in2code.de>
2025-07-26 17:01:46 +0200

    [TASK] Move icon registration to Configuration folder
    That allows us to overrule the settings in a project with in2template

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'extension-instagram' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:instagram/Resources/Public/Icons/Extension.svg',
+    ]
+];
